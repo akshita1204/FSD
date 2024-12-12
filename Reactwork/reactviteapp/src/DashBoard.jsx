e834aac4e9ee9aa3b60d3ca06f7111c8c741bc02
akshita1204 <akshitatyagi04@gmail.com>
2024-12-12 13:04:55 +0530

    commit changes

--- Reactwork/reactviteapp/src/DashBoard.jsx
+++ Reactwork/reactviteapp/src/DashBoard.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
 function DashBoard() {
+    const[data,setData]=useState([]);
+
+function getonlineData()
+{
+    const onlinedata=fetch('https://dummyjson.com/recipes');
+    onlinedata.then((response)=>{
+      response.json().then((res)=>
+    {
+   console.log(res.recipes);
+   setData(res.recipes);
+    })
+    })
+}
+getonlineData()
+
   return (
-    <div>DashBoard</div>
+    <div >DashBoard
+        {/* <div>{JSON.stringify(data)}</div> */}
+        {
+            data.map((ele)=>
+            {
+                return(
+                    <div style={{border:'2px solid black'}}>
+                        <div><img src={ele.image} height="150" width="150"/></div>
+                        <div>{ele.name}</div>
+                    </div>
+                )
+            })
+        }
+    </div>
   )
 }
 
