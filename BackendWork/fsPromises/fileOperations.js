12d3cf3a80eae8ae500974f5570733add6417a20
akshita1204 <akshitatyagi04@gmail.com>
2025-02-11 10:25:59 +0530

    commit changes:

--- BackendWork/fsPromises/fileOperations.js
+++ BackendWork/fsPromises/fileOperations.js
@@ -1,3 +1,12 @@
 const promise=require('fs').promises;
 const fsp=promise.writeFile('data.txt',"Hello using fs promises to write the data");
-console.log(fsp)
+fsp.then(()=>
+{
+    console.log("data written successfully");
+}).catch((err)=>
+{
+
+}).finally(()=>
+{
+    
+})
