bfdc3691801edcc8491088fa3154c0bf4fac3ef5
akshita1204 <akshitatyagi04@gmail.com>
2025-02-11 10:13:21 +0530

    commit changes

--- BackendWork/fsCallback/fileOperations.js
+++ BackendWork/fsCallback/fileOperations.js
@@ -1,5 +1,16 @@
 const fs=require('fs');
 fs.writeFile('data.txt', "Welcome to FSD session", ()=>
 {
-    
+    console.log("Data written Successfully");
 })
+fs.readFile('data.txt',(error,data)=>
+{
+ if(error)
+ {
+    console.log("Error while reading the data")
+ }
+ else
+ {
+    console.log(data.toString());
+ }
+} )
