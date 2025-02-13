ae64be99af4eb284cf860ab2663104c41ec16018
akshita1204 <akshitatyagi04@gmail.com>
2025-02-13 12:43:02 +0530

    http server started

--- BackendWork/HttpModule/index.js
+++ BackendWork/HttpModule/index.js
@@ -1,6 +1,13 @@
 const http=require('http');
 const PORT=2527;
-http.createServer((req,res)=>{
+const server=http.createServer((req,res)=>{
 
+    console.log("Inside server");
+  res.write("Welcome to the Node Server");
+  res.end("server ended")
 })
-server.listen()
+server.listen(PORT,()=>
+{
+    console.log("Server is running on the " +PORT)
+})
+//reserved port numbers are => upto 1024
