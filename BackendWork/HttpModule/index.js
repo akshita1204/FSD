14a5fb19ad574042e477279bc109f1373962a54a
akshita1204 <akshitatyagi04@gmail.com>
2025-02-18 09:16:34 +0530

    commit changes

--- BackendWork/HttpModule/index.js
+++ BackendWork/HttpModule/index.js
@@ -3,9 +3,14 @@ const PORT=2527;
 const server=http.createServer((req,res)=>{
 
     console.log("Inside server");
-    res.setHeader('Content-type',"text/html"); //to use the html tag i
-    res.write("<h2 style='background:yellow'>Welcome to the Node Server</h2> ");
-    res.end("<h2>server ended</h2>")
+    if(req.url=='/' && req.method=='GET')
+    {
+        res.setHeader('Content-type',"text/html"); //to use the html tag i
+        //console.log(Object.keys(req)) 
+        console.log(req.url+" "+req.method);
+        res.write("<h2 style='background:yellow'>Welcome to the Node Server</h2> ");
+        res.end("<h2>server ended</h2>")
+    }
 })
 server.listen(PORT,()=>
 {
