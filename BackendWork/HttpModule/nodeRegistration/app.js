c3ff6460ae1356762ed61f33fc4b00faaa940bc0
akshita1204 <akshitatyagi04@gmail.com>
2025-02-27 12:13:34 +0530

    commit changes

--- BackendWork/HttpModule/nodeRegistration/app.js
+++ BackendWork/HttpModule/nodeRegistration/app.js
@@ -17,7 +17,10 @@ const server=http.createServer((req,res)=>
     {
        const {name,email,password} = JSON.parse(body);
        console.log(name)
-       await fs.writeFile('studentss.json',JSON.stringify({name,email,password}));
+       const fdata=await fs.readFile('studentss.json',{encoding:'utf-8'});
+       arr=JSON.parse(fdata);
+       arr.push({name,email,password});
+      await fs.writeFile('studentss.json',JSON.stringify(arr,null,2));
     })
 
     res.end(JSON.stringify({"message":"/register api hit successfully"}));
